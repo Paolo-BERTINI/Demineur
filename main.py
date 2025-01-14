4f18431d9fa453bad4bac280e18a8549df722fdb
Paolo BERTINI <paullus64000@gmail.com>
2025-01-14 15:45:23 +0100

    indentation

--- main.py
+++ main.py
@@ -175,8 +175,8 @@ root.bind("<Escape>", lambda event: action_quitter(event, root))
 
 # On affiche la solution pendant une seconde avec la touche "t" pour triche.
 root.bind("<Insert>", lambda event: action_afficher_solution(event, plateau_courant, grille, largeur_case, hauteur_case,
-                                                            DRAPEAU, drapeau_img, QUESTION, question_img, INCONNU,
-                                                            mine_img, inconnu_img, PERDU, perdu_img))
+                                                             DRAPEAU, drapeau_img, QUESTION, question_img, INCONNU,
+                                                             mine_img, inconnu_img, PERDU, perdu_img))
 
 # On obtient de l'aide avec la touche "suppr".
 root.bind("<Delete>", lambda event: action_aide(event))
@@ -192,16 +192,16 @@ root.bind("<Delete>", lambda event: action_aide(event))
 
 # On ouvre la case avec un clic gauche
 grille.bind("<Button-1>", lambda clic: action_clic_decouvre(clic, plateau_courant, largeur_case, hauteur_case,
-                                                             plateau_courant, INCONNU, PERDU, grille, DRAPEAU,
-                                                             drapeau_img, QUESTION, question_img, mine_img,
-                                                             inconnu_img, perdu_img, game_over))
+                                                            plateau_courant, INCONNU, PERDU, grille, DRAPEAU,
+                                                            drapeau_img, QUESTION, question_img, mine_img,
+                                                            inconnu_img, perdu_img, game_over))
 
 
 # On place un drapeau ou un point d'interrogation avec un clic droit
 ### QUESTION : écrire l'instruction correspondante
 grille.bind("<Button-3>", lambda clic: action_clic_drapeau_question(clic, largeur_case, hauteur_case, plateau_courant, INCONNU,
-                                                                     DRAPEAU, QUESTION, grille, drapeau_img, question_img,
-                                                                     mine_img, inconnu_img, PERDU, perdu_img, game_over))
+                                                                    DRAPEAU, QUESTION, grille, drapeau_img, question_img,
+                                                                    mine_img, inconnu_img, PERDU, perdu_img, game_over))
 
 
 
