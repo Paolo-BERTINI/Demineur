b19b93b2aced92cf7886ce9f7011d8d9fa1fb786
Paolo BERTINI <paullus64000@gmail.com>
2025-01-15 10:27:22 +0100

    gestion evenement

--- main.py
+++ main.py
@@ -13,8 +13,9 @@
 # On importe toute la bibliothèque tkinter pour l'interface graphique.
 
 from evenements.evenements import *
+from evenements.gestion import gestion_events
 from plateau.grille import dessine_plateau
-from plateau.logistique import genere_plateau, dans_plateau, gagne, decouvre_case, compte_mines_solution
+from plateau.logistique import genere_plateau
 from tkinter import *
 
 
@@ -85,54 +86,6 @@ grille = Canvas(root , width=largeur*(largeur_case+1)+1 , \
 grille.pack()
 
 
-
-##############################################################################
-##############################################################################
-###                       GESTION DES ÉVÉNEMENTS                           ###
-##############################################################################
-##############################################################################
-
-# On va utiliser la méthode bind pour les événements à gérer.
-# La syntaxe est la suivante :  bind('evenement', commande)
-# 'evenement' doit être un événement (saisie d'une touche, clic de souris, ...)
-# commande est une fonction prenant un evenement en argument dans sa définition
-# Le paramètre evenement est omis lors de l'appel.
-
-# On quitte le programme avec la touche "échap".
-root.bind("<Escape>", lambda event: action_quitter(event, root))
-
-# On affiche la solution pendant une seconde avec la touche "t" pour triche.
-root.bind("<Insert>", lambda event: action_afficher_solution(event, plateau_courant, grille, largeur_case, hauteur_case,
-                                                             DRAPEAU, drapeau_img, QUESTION, question_img, INCONNU,
-                                                             mine_img, inconnu_img, PERDU, perdu_img))
-
-# On obtient de l'aide avec la touche "suppr".
-root.bind("<Delete>", lambda event: action_aide(event))
-### QUESTION : écrire les deux instructions correspondantes :
-
-
-# Pour la souris, quelques événements possibles (il en existe d'autres) :
-# <Button-1>           : Clic gauche
-# <Button-2>           : Clic milieu 
-# <Button-3>           : Clic droit
-# <Double-Button-1>    : Double clic droit
-# <Double-Button-2>    : Double clic gauche
-
-# On ouvre la case avec un clic gauche
-grille.bind("<Button-1>", lambda clic: action_clic_decouvre(clic, plateau_courant, largeur_case, hauteur_case,
-                                                            plateau_courant, INCONNU, PERDU, grille, DRAPEAU,
-                                                            drapeau_img, QUESTION, question_img, mine_img,
-                                                            inconnu_img, perdu_img, game_over))
-
-
-# On place un drapeau ou un point d'interrogation avec un clic droit
-### QUESTION : écrire l'instruction correspondante
-grille.bind("<Button-3>", lambda clic: action_clic_drapeau_question(clic, largeur_case, hauteur_case, plateau_courant, INCONNU,
-                                                                    DRAPEAU, QUESTION, grille, drapeau_img, question_img,
-                                                                    mine_img, inconnu_img, PERDU, perdu_img, game_over))
-
-
-
 ##############################################################################
 ##############################################################################
 ###                     LANCEMENT DU PROGRAMME                             ###
@@ -145,6 +98,10 @@ plateau_courant = genere_plateau(largeur, hauteur, probabilite_mine, INCONNU)
 # On dessine le plateau de jeu :
 dessine_plateau(plateau_courant, grille, largeur_case, hauteur_case,DRAPEAU,drapeau_img,
                 QUESTION, question_img, INCONNU, mine_img, inconnu_img, PERDU, perdu_img)
+
+# On gère les évènements :
+gestion_events(root, plateau_courant, grille, largeur_case, hauteur_case, DRAPEAU, drapeau_img,
+               QUESTION, question_img, INCONNU, mine_img, inconnu_img, PERDU, perdu_img, game_over)
 
 # Affiche la fenêtre et lance la boucle de gestion des événements :
 grille.mainloop()
